e8ca1669e04d00bc21bdf3c2da8b0d3aa96c6304
Yoel <63026075+YoelSolca@users.noreply.github.com>
2021-09-05 16:37:50 -0300

    Portfolio - V1

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let toggle = document.querySelector('.toggle');
 let topbar = document.querySelector('.topbar');
 let navigation = document.querySelector('.navigation');
 let main = document.querySelector('.main');
+let themeSwitch = document.querySelector('.themeSwitch');
+let body = document.querySelector('body');
 
 
 toggle.onclick = function() {
@@ -9,4 +11,19 @@ toggle.onclick = function() {
     topbar.classList.toggle('active');
     navigation.classList.toggle('active');
     main.classList.toggle('active');
+}
+
+/* Fix menu active class issue for mobile devices*/
+function toggleMenu() {
+    let navigation = document.querySelector('.navigation');
+    let main = document.querySelector('.main');
+
+    navigation.classList.remove('active');
+    main.classList.remove('active');
+
+}
+
+/* Theme switch toggle*/
+themeSwitch.onclick = function() {
+    body.classList.toggle('dark');
 }
